6d451059f654c2aa7d46c78493e70752ed6a3080
辣橙 <68316902+laorange@users.noreply.github.com>
2022-09-11 15:38:48 +0800

    feat: CopyrightDiv

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import CopyrightDiv from "./CopyrightDiv.vue";
 
 </script>
 
 <template>
   <img src="../assets/auto-editor_banner.png" alt="auto-editor_banner.png"
        class="auto-editor-banner"/>
+
+  <CopyrightDiv project-url="https://github.com/laorange/auto-editor-assistant"
+                addition="For auto-editor v22.35.3"/>
 </template>
 
 <style scoped>
